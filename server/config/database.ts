ab75c5ca9dfcffe3500ca435859b9e8bb992841e
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-06-07 12:23:57 +0530

    MongoDB Connection

---
+++ server/config/database.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const URI = process.env.MONGODB_URL
+
+mongoose.connect(`${URI}`,{
+    
+},(err)=>{
+    if(err) throw err;
+    console.log('MongoDB connection')
+})
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,34 @@
-const test: string = "hello world"
+import dotenv from 'dotenv'
+dotenv.config()
 
-console.log(test)
+
+
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+
+
+//Middleware 
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(cookieParser())
+
+//Routes
+
+app.get('/',(req, res) => {
+    res.json({msg: 'Hello World!'})
+})
+
+//Database
+
+import './config/database'
+
+//Server Listinings
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log('Server is Running on port: ', PORT)
+})
